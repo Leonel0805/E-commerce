06d46abef6a041375bc1efbbbdba9f9991af9e22
Leonel Rodriguez <114777251+Leonel0805@users.noreply.github.com>
2023-07-08 16:22:48 -0300

    agregando estilos base

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def inicio(request):
+    if request.method == 'GET':
+        return render(request, 'inicio.html')
